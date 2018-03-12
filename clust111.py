229b7a7d9b60a114d4dadc2fbd94bbf97a7c2c1a
maxM <mw159159@gmail.com>
2018-03-12 18:52:53 +0500

    first commit

---
+++ clust111.py
@@ -0,0 +1,221 @@
+# Author: Gael Varoquaux <gael.varoquaux@normalesup.org>, Brian Cheung
+# License: BSD 3 clause
+
+import time
+
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import cv2
+
+from sklearn.feature_extraction import image
+from sklearn.cluster import spectral_clustering
+
+from sklearn.cluster import MiniBatchKMeans, KMeans
+
+from sklearn.cluster import DBSCAN
+from sklearn import metrics
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.preprocessing import StandardScaler
+
+from pickle import load, dump
+
+from wriply import write_plyTri
+
+def loadPoClo(pointsC):
+
+# load the raccoon face as a numpy array
+#try:  # SciPy >= 0.16 have face in misc
+#    from scipy.misc import face
+#    face = face(gray=True)
+#except ImportError:
+#    face = sp.face(gray=True)
+#print('load image...')
+#face = cv2.pyrDown(cv2.imread ('stul/disp/1d_2208_2b3(2).jpg'))
+#face = cv2.imread ('stul/disp/1d_2208_2b3(2).jpg')
+    try:
+        print('loaded from pickle')
+        pointsC = load(open("pointsC.p","rb"))
+    except FileNotFoundError:
+        print('load data from file...')
+        points2 = np.loadtxt("out3", delimiter=" ")
+        pointsC = np.hstack((points2[:,0:1],points2[:,1:2],points2[:,2:3]))
+        dump(pointsC,open("pointsC.p","wb"))
+print('invert X Y...')
+#exit(0)
+#max2 = pointsC[:,0].max()
+#pointsC[:,0]= max2 - pointsC[:,0]
+#max2 = pointsC[:,1:2].max()
+#pointsC[:,1:2]= max2 - pointsC[:,1:2]
+#pointsC = points2[:,0:2]
+#print(pointsC[0])
+#exit(0)
+# Resize it to 10% of the original size to speed up the processing
+#print('resize image...')
+#face = sp.misc.imresize(face, 0.10) / 255.
+#cv2.imshow('face', face)
+#cv2.waitKey()
+# Convert the image into a graph with the value of the gradient on the
+# edges.
+
+
+# #############################################################################
+# Generate sample data
+#centers = [[1, 1], [-1, -1], [1, -1]]
+#X, labels_true = make_blobs(n_samples=750, centers=centers, cluster_std=0.4,
+#                            random_state=0)
+print('scaller...')
+X = pointsC#StandardScaler().fit_transform(pointsC)#[:100000])
+maxX=X[::,0:3].max()
+X[::,0] = X[::,0]/maxX
+X[::,1] = X[::,1]/maxX
+X[::,2] = X[::,2]/maxX
+# #############################################################################
+try:
+    db = load(open("dbs.pickle","rb"))
+    print('DB loaded from pickle')
+except FileNotFoundError:
+    print('DBSCAN...')
+    t0 = time.time()
+    db = DBSCAN(eps=0.01, min_samples=100).fit(X)
+    t_dbs = time.time() - t0
+    print("Time: %.2f" % t_dbs)
+    dump(db,open("dbs.pickle","wb"))
+# Compute DBSCAN
+
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
+labels = db.labels_
+
+# Number of clusters in labels, ignoring noise if present.
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+
+print('Estimated number of clusters: %d' % n_clusters_)
+
+from matplotlib import pyplot
+from mpl_toolkits.mplot3d import Axes3D
+fig = pyplot.figure()
+ax = Axes3D(fig)
+#print(labels.shape)
+#exit(0)
+try:
+    X2 = load(open("X2.pickle","rb"))
+    print('X2 loaded from pickle')
+except FileNotFoundError:
+    print('start X2...')
+    t0 = time.time()
+    X2=np.array([0,0,0,0,0,0])
+    for i in range(0,labels.shape[0]):
+        if labels[i]==-1:
+            X[i]=[0,0,0]
+        else:
+            #if labels[i] in range(10,50):
+            X2 = np.vstack((X2,np.array([X[i,0],X[i,1],X[i,2],labels[i],200,100])))
+    t_dbs = time.time() - t0
+    dump(X2,open("X2.pickle","wb"))
+    print("end X2   Time: %.2f" % t_dbs)
+X3=np.array([0,0,0,0,0,0])
+X3 = X2
+#X2 = X2[::30,::]
+#for i in range(0,X2.shape[0]):
+    #if X2[i,3] in range(30,31):#int(X2[::,3].max())):
+    #    X3 = np.vstack((X3,np.array(X2[i])))
+#    X3 = np.vstack((X3,np.array(X2[i])))
+X3[::,0:3] = X3[::,0:3]*100
+
+#ax.scatter(X[:,0],X[:,1],X[:,2],c=labels)
+#ax.scatter(X3[:,0],X3[:,1],X3[:,2],c=X3[:,3])
+#pyplot.show()
+write_plyTri('out112.ply', X3)
+print('%s saved' % 'out112.ply')
+exit(0)
+#print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
+#print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
+#print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
+#print("Adjusted Rand Index: %0.3f"
+#      % metrics.adjusted_rand_score(labels_true, labels))
+#print("Adjusted Mutual Information: %0.3f"
+#      % metrics.adjusted_mutual_info_score(labels_true, labels))
+#print("Silhouette Coefficient: %0.3f"
+#      % metrics.silhouette_score(X, labels))
+
+# #############################################################################
+# Plot result
+import matplotlib.pyplot as plt
+
+# Black removed and is used for noise instead.
+unique_labels = set(labels)
+colors = [plt.cm.Spectral(each)
+          for each in np.linspace(0, 1, len(unique_labels))]
+for k, col in zip(unique_labels, colors):
+    if k == -1:
+        # Black used for noise.
+        col = [0, 0, 0, 1]
+
+    class_member_mask = (labels == k)
+
+    xy = X[class_member_mask & core_samples_mask]
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+             markeredgecolor='k', markersize=5)
+
+    xy = X[class_member_mask & ~core_samples_mask]
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+             markeredgecolor='k', markersize=1)
+
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.show()
+exit(0)
+#----------------------------------------------------------
+print('graph image...')
+graph = image.img_to_graph(face)
+
+# Take a decreasing function of the gradient: an exponential
+# The smaller beta is, the more independent the segmentation is of the
+# actual image. For beta=1, the segmentation is close to a voronoi
+beta = 5
+eps = 1e-6
+print('exp image...')
+graph.data = np.exp(-beta * graph.data / graph.data.std()) + eps
+
+#X=face
+print(face.shape)
+exit(0)
+# k_means = KMeans(init='k-means++', n_clusters=3, n_init=10)
+# t0 = time.time()
+# k_means.fit(X)
+# t_batch = time.time() - t0
+#
+# mbk = MiniBatchKMeans(init='k-means++', n_clusters=3, batch_size=batch_size,
+#                       n_init=10, max_no_improvement=10, verbose=0)
+# t0 = time.time()
+# mbk.fit(X)
+# t_mini_batch = time.time() - t0
+
+# Apply spectral clustering (this step goes much faster if you have pyamg
+# installed)
+N_REGIONS = 10
+
+#Visualize the resulting regions
+print('start clustering image...')
+for assign_labels in ('discretize', 'kmeans'):#, 'discretize'):
+    t0 = time.time()
+    print('spectral clustering graph...')
+    labels = spectral_clustering(graph, n_clusters=N_REGIONS,
+                                 assign_labels=assign_labels, random_state=1)
+    t1 = time.time()
+    labels = labels.reshape(face.shape)
+
+    plt.figure(figsize=(5, 5))
+    plt.imshow(face, cmap=plt.cm.gray)
+    for l in range(N_REGIONS):
+        plt.contour(labels == l, contours=1,
+                    colors=[plt.cm.spectral(l / float(N_REGIONS))])
+    plt.xticks(())
+    plt.yticks(())
+    title = 'Spectral clustering: %s, %.2fs' % (assign_labels, (t1 - t0))
+    print(title)
+    plt.title(title)
+plt.show()
+
+if __name__ == '__main__':
+    loadPoClo(PointC)
---
+++ stereo_match2.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python
+
+'''
+Simple example of stereo image matching and point cloud generation.
+
+Resulting .ply file cam be easily viewed using MeshLab ( http://meshlab.sourceforge.net/ )
+'''
+
+# Python 2/3 compatibility
+from __future__ import print_function
+
+import numpy as np
+import cv2
+import sys
+from math import acos,atan,sqrt,pow,sin,cos,degrees,radians
+
+def toSpherical_np(xyz):
+    ptsnew = np.hstack((xyz, np.zeros(xyz.shape)))
+    xy = xyz[:,0]**2 + xyz[:,1]**2
+    ptsnew[:,3] = np.sqrt(xy + xyz[:,2]**2)
+    ptsnew[:,4] = np.arctan2(np.sqrt(xy), xyz[:,2]) # for elevation angle defined from Z-axis down
+    #ptsnew[:,4] = np.arctan2(xyz[:,2], np.sqrt(xy)) # for elevation angle defined from XY-plane up
+    ptsnew[:,5] = np.arctan2(xyz[:,0], xyz[:,1])
+    return ptsnew
+def toSpherical_np2(xyz):
+    ptsnew = np.hstack((xyz, np.zeros(xyz.shape)))
+    xy = xyz[:,0]**2 + xyz[:,1]**2
+    ptsnew[:,6] = np.sqrt(xy + xyz[:,2]**2)
+    ptsnew[:,7] = np.arctan2(np.sqrt(xy), xyz[:,2]) # for elevation angle defined from Z-axis down
+    #ptsnew[:,4] = np.arctan2(xyz[:,2], np.sqrt(xy)) # for elevation angle defined from XY-plane up
+    ptsnew[:,8] = np.arctan2(xyz[:,0], xyz[:,1])
+    return ptsnew
+
+NaN = float('nan')
+ply_header = '''ply
+format ascii 1.0
+element vertex %(vert_num)d
+property float x
+property float y
+property float z
+property uchar red
+property uchar green
+property uchar blue
+end_header
+'''
+
+def write_ply(fn, verts, colors):
+    verts = verts.reshape(-1, 3)
+    colors = colors.reshape(-1, 3)
+    verts = np.hstack([verts, colors])
+    with open(fn, 'wb') as f:
+        f.write((ply_header % dict(vert_num=len(verts))).encode('utf-8'))
+        np.savetxt(f, verts, fmt='%f %f %f %d %d %d ')
+
+
+if __name__ == '__main__':
+    print('loading images...')
+    #imgL = cv2.pyrDown( cv2.imread('../data/aloeL.jpg') )  # downscale images for faster processing
+    #imgR = cv2.pyrDown( cv2.imread('../data/aloeR.jpg') )
+    #imgL = cv2.pyrDown( cv2.imread(sys.argv[1]) )  # downscale images for faster processing
+    #imgR = cv2.pyrDown( cv2.imread(sys.argv[2]) )
+    imgL = cv2.imread(sys.argv[1])
+    imgR = cv2.imread(sys.argv[2])
+    #imgL = cv2.pyrDown( cv2.imread('stul/1r.JPG') )  # downscale images for faster processing
+    #imgR = cv2.pyrDown( cv2.imread('stul/1l.JPG') )
+
+    # disparity range is tuned for 'aloe' image pair
+    window_size = 3
+    min_disp = 16
+    num_disp = 112-min_disp
+    stereo = cv2.StereoSGBM_create(minDisparity = min_disp,
+        numDisparities = num_disp,
+        blockSize = int(sys.argv[4]),
+        P1 = 8*3*window_size**2,
+        P2 = 32*3*window_size**2,
+        disp12MaxDiff = 1,
+        uniquenessRatio = 10,
+        speckleWindowSize = 100,
+        speckleRange = 32
+    )
+
+    print('computing disparity...')
+    disp = stereo.compute(imgL, imgR).astype(np.float32) / 16.0
+    #print(disp[1000][500],imgR[1000][500])
+    #exit(0)
+    cv2.imwrite(sys.argv[3], disp*2)
+    h, w = disp.shape[:2]
+    open('out3','w').write('')
+    #mydisp_spher = []
+    for x in range(-int(w/2),int(w/2), 5):
+        for y in range(-int(h/2),int(h/2), 5):
+            try:
+                z = disp[y+int(h/2)][x+int(w/2)]
+            except IndexError:
+                print (w,h,x,y)
+            if z>=16 :
+                try:
+                    #print (w,h,x,y,z)
+                    x2 = radians(x*(44/2)/int(w/2))
+                    ##y2 = radians(y*(44/2)/int(w/2))
+                    ##z2 = 2+(10-2)*((111-z)/(111-16)) #0.0074*z+1.8818
+                    ##ro = z2 #sqrt(pow(x2,2)+pow(y2,2)+pow(z2,2))
+                    ##phi = x*(44/2)/int(w/2)#acos(z2/ro) #
+                    #phi = degrees(acos(z2/ro))
+                    ##psi = y*(44/2)/int(w/2)#atan(y2/x2) #
+                    #if ro<5:
+                    #    ro=ro + pow(ro,(5-ro))*0.13
+                    #else:
+                    #    ro=sqrt(pow(ro,2)+pow(x/280,2))
+                    #    ro=sqrt(pow(ro,2)+pow(y/(280),2))
+                    #if psi<0:
+                    #    ro=0;phi=0;psi=0 #ro = ro*1.7
+                    #psi = degrees(atan(y2/x2))
+                    #ro = z2 - sin(radians(phi)) + sin(radians(psi))
+                except (ZeroDivisionError,ValueError) as e:
+                    import sys
+                    print(x,y,z,ro,phi,psi,file=sys.stderr)
+                    ro, phi, psi = NaN, NaN, NaN
+                #mydisp_spher += [[ro,phi,psi]]
+                #print(x,y,z,ro,phi,psi)
+                #if -1<phi<1:
+                #open('out3','a').write("%s %s %s %s\n" % (phi,psi,ro,ro/cos(radians(phi))))
+            else:
+                z=0
+    print('generating 3d point cloud ...',)
+    h, w = imgL.shape[:2]
+    f = 0.6*w                          # guess for focal length
+    Q = np.float32([[1, 0, 0, -0.5*w],
+                    [0,-1, 0,  0.5*h], # turn points 180 deg around x-axis,
+                    [0, 0, 0,     -f], # so that y-axis looks up
+                    [0, 0, 1,      0]])
+    points = cv2.reprojectImageTo3D(disp, Q)
+    #print(points.shape)
+    #print(points[0,0])
+    points2 = np.dstack((points,imgR[:,:,0:3]))
+    #points2 = np.dstack((points[::,::,0:1]))
+    #print(points2.shape)
+    #print(points2[0,0])
+    mask = disp > disp.min()
+    #print(disp.shape)
+    #print(disp[0,0])
+    out_points = points2[mask]
+    #print(out_points.shape)
+    #print(out_points[0])
+    #exit(0)
+    #np.savetxt("out4", out_points, delimiter=" ")
+    #points2 = toSpherical_np(out_points)
+    #np.savetxt("out3", points2[::10], delimiter=" ")
+
+    print('convert 3d to spherical...')
+    #for i in out_points:
+    #    if i del
+    out_points[:,0]+=100#-1*out_points[:,0].min()
+    out_points[:,1]+=100#-1*out_points[:,1].min()
+    out_points[:,2]+=100#-1*out_points[:,2].min()
+    print(out_points[:,0].min(),out_points[:,1].min(),out_points[:,2].min())
+    print(out_points[:,0].max(),out_points[:,1].max(),out_points[:,2].max())
+    points2 = toSpherical_np2(out_points)
+    np.savetxt("out3", points2[::5,0:9], delimiter=" ")
+    exit(0)
+    #open('out3','a').write("%s\n%s" % (points[0],points2[0]))
+    exit(0)
+    #cv2.bilateralFilter(disp, disp_filt,)
+
+    print('generating 3d point cloud 2...',)
+    for i3 in range(imgL.shape[:1]):
+        x3 = mydisp_spher[i3]
+        print(x3,y3,z3,110,80,110,file='out2.ply')
+
+    write_ply('out2.ply', out_points, out_colors)
+    print('%s saved' % 'out2.ply')
+
+    print('generating 3d point cloud...',)
+    h, w = imgL.shape[:2]
+    f = 0.6*w                          # guess for focal length
+    Q = np.float32([[1, 0, 0, -0.5*w],
+                    [0,-1, 0,  0.5*h], # turn points 180 deg around x-axis,
+                    [0, 0, 0,     -f], # so that y-axis looks up
+                    [0, 0, 1,      0]])
+    points = cv2.reprojectImageTo3D(disp, Q)
+    colors = cv2.cvtColor(imgL, cv2.COLOR_BGR2RGB)
+    mask = disp > disp.min()
+    out_points = points[mask]
+    out_colors = colors[mask]
+    out_fn = 'out.ply'
+    write_ply('out.ply', out_points, out_colors)
+    print('%s saved' % 'out.ply')
+
+    #cv2.imshow('disparity', (disp-min_disp)/num_disp)
+    #cv2.waitKey()
+    #cv2.destroyAllWindows()
